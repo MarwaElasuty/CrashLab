28f0ad0b37abbdd658bf05c56d55b9dd4f1844e9
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2024-03-10 12:23:58 +0200

    dsa

--- index.js
+++ index.js
@@ -35,3 +35,37 @@ function hideDiv(Div) {
         hide.style.display = "none";
     }
 }
+
+const swiperEl = document.querySelector("swiper-container");
+
+      const params = {
+        injectStyles: [
+          `
+      .swiper-pagination-bullet {
+        width: 20px;
+        height: 20px;
+        text-align: center;
+        line-height: 20px;
+        font-size: 12px;
+        color: #000;
+        opacity: 1;
+        background: rgba(0, 0, 0, 0.2);
+      }
+
+      .swiper-pagination-bullet-active {
+        color: #fff;
+        background: red;
+      }
+      `,
+        ],
+        pagination: {
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+          },
+        },
+      };
+
+      Object.assign(swiperEl, params);
+
+      swiperEl.initialize();
