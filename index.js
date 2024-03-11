47ca0a2668a3c65cb783e9af4c258cbc09cb5e9a
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2024-03-11 14:03:09 +0200

    fsdfs

--- index.js
+++ index.js
@@ -66,3 +66,36 @@ $(document).ready(function () {
 
 
 
+const swiperEl = document.querySelector("swiper-container");
+
+      const params = {
+        injectStyles: [
+          `
+      .swiper-pagination-bullet {
+        width: 20px;
+        height: 20px;
+        text-align: center;
+        line-height: 20px;
+        font-size: 18px;
+        color: #000;
+        opacity: 1;
+        background: white;
+      }
+
+      .swiper-pagination-bullet-active {
+        color: #fff;
+        background: #ffad00;
+      }
+      `,
+        ],
+        pagination: {
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+          },
+        },
+      };
+
+      Object.assign(swiperEl, params);
+
+      swiperEl.initialize();
