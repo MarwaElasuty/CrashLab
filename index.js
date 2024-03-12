fe332ef26592689b19bb4e306550f8dc399e64bf
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2024-03-12 20:36:43 +0200

    ew

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+const swiperEl = document.querySelector('swiper-container')
 
+    const params = {
+      injectStyles: [`
+      .swiper-pagination-bullet {
+        width: 20px;
+        height: 20px;
+        text-align: center;
+        line-height: 20px;
+        font-size: 18px;
+        color: #000;
+        opacity: 1;
+        background: rgba(0, 0, 0, 0.2);
+      }
+
+      .swiper-pagination-bullet-active {
+        color: #fff;
+      }
+      `],
+      pagination: {
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+      },
+    }
+
+    Object.assign(swiperEl, params)
+
+    swiperEl.initialize();
 
 function appear() {
     document.querySelector(".ul-list").classList.toggle("display");
@@ -61,37 +90,3 @@ $(document).ready(function () {
 });
 
 
-
-const swiperEl = document.querySelector("swiper-container");
-
-      const params = {
-        injectStyles: [
-          `
-      .swiper-pagination-bullet {
-        width: 20px;
-        height: 20px;
-        text-align: center;
-        line-height: 20px;
-        font-size: 18px;
-        color: #000;
-        opacity: 1;
-        background: white;
-      }
-
-      .swiper-pagination-bullet-active {
-        color: #fff;
-        background: #ffad00;
-      }
-      `,
-        ],
-        pagination: {
-          clickable: true,
-          renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + (index + 1) + "</span>";
-          },
-        },
-      };
-
-      Object.assign(swiperEl, params);
-
-      swiperEl.initialize();
