10747427ad9a0bdb30ae2c816e0b3afefbe4ea9d
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2024-03-24 15:21:22 +0200

    awe

--- index.js
+++ index.js
@@ -132,43 +132,7 @@ function change_arabic() {
     aboutUsFlexBefore.classList.remove('about-us-flex .first-left-img-flex::before');
   }
 }
-// function change_arabic(){
 
-//   document.getElementById('body').style.direction = "rtl";
-//   document.querySelector(".customer-stories-right").classList.add("arabic-customers");
-//   document.querySelector('.customer-stories-right').classList.remove('right-column');
-
-//   document.querySelector(".about-container").classList.add("arabic-about-container");
-//   document.querySelector('.about-container').classList.remove('about-container');
-
-//   document.querySelector(".desc-onee").classList.add("arabic-desc-onee");
-//   document.querySelector('.desc-onee').classList.remove('desc-onee');
-
-//   document.querySelector(".desc-two").classList.add("arabic-desc-two");
-//   document.querySelector('.desc-two').classList.remove('desc-two');
-
-//   document.querySelector(".contact-form-container").classList.add("arabic-contact-black");
-//   document.querySelector('.contact-form-container').classList.remove('contact-form-container');
-
-
-//   document.querySelector(".innovative-cards .left-column").classList.add("arabic-innovative-cards-left-column");
-//   document.querySelector('.innovative-cards .left-column').classList.remove('left-column');
-
-//   document.querySelector(".added-real-text").classList.add("arabic-added-real-text");
-//   document.querySelector('.added-real-text').classList.remove('added-real-text');
-
-//   document.querySelector(".last-added-card").classList.add("arabic-last-added-card");
-//   document.querySelector('.last-added-card').classList.remove('last-added-card');
-
-
-//   document.querySelectorAll(".added-value-cards p").forEach((para)=>{
-//     para.classList.add("arabic-added-value-cards");
-//     para.classList.remove('added-value-cards');
-//   });
-
-//   document.querySelector(".about-us-flex .first-left-img-flex::before").classList.add("arabic-first-left-img-flex-before");
-//   document.querySelector('.about-us-flex .first-left-img-flex::before').classList.remove('about-us-flex .first-left-img-flex::before');
-//  }
 
 function change_english() {
   const bodyElement = document.getElementById('body');
